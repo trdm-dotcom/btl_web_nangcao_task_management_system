33fb9218fba01b504cc6b2c969698941e9b943b3
Duc Minh <tranfminh@gmail.com>
2023-03-02 04:25:40 +0000

    ProjectClose

--- UI/project/ProjectClose.aspx.cs
+++ UI/project/ProjectClose.aspx.cs
@@ -5,12 +5,15 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using btl_web_nangcao_task_management_system.model;
+using btl_web_nangcao_task_management_system.Repositories;
 using btl_web_nangcao_task_management_system.model.db;
 
 namespace btl_web_nangcao_task_management_system.UI.project
 {
     public partial class ProjectClose : System.Web.UI.Page
     {
+        String connectionString = ConfigurationManager.ConnectionStrings["connTaskManagementSystem"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -18,7 +21,28 @@ namespace btl_web_nangcao_task_management_system.UI.project
 
         protected void closeButton_Click(object sender, EventArgs e)
         {
+            if(!CheckInputValues())
+            {
+                return;
+            }
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.Connection = connection;
 
+                Project project = new Project();
+                project.Id = int.Parse(projectDropDownList.SelectedItem.Value);
+                project.status = ProjectStatus.CLOSE;
+            }
+            catch(Exception ex)
+            {
+                errorMessage.Text = "Internal error server";
+            }
+            finally {
+                connection.close();
+            }
         }
 
         protected void projectDropDownList_SelectedIndexChanged(object sender, EventArgs e)
@@ -27,15 +51,22 @@ namespace btl_web_nangcao_task_management_system.UI.project
             {
                 return;
             }
+            SqlConnection connection = new SqlConnection(connectionString);
             try
             {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.Connection = connection;
+
                 Project project = new Project();
                 project.Id = int.Parse(projectDropDownList.SelectedItem.Value);
-                project.status = ProjectStatus.CLOSE;
             }
             catch(Exception ex)
             {
                 errorMessage.Text = "Internal error server";
+            }
+            finally {
+                connection.close();
             }
         }
 
